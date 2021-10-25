2aa78ad0d43ed6ea3af4a958d36023e2956e480e
garrettdick <garrettd0511@gmail.com>
2021-10-25 17:42:09 -0500

    completed and test fetch request to our db.json

--- phase2-project/src/App.js
+++ phase2-project/src/App.js
@@ -1,7 +1,18 @@
+import React, {useState, useEffect} from 'react'
+import {Route, Switch} from 'react-router-dom'
 
-import './App.css';
+// import './App.css';
 
 function App() {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+   fetch('http://localhost:4000/blogs')
+   .then(resp => resp.json())
+   .then(allBlogs => setBlogs(allBlogs))
+  },[])
+  console.log(blogs)
+
   return (
     <div>
       <h1>Hello world!</h1>
