e17f35bf9c9b93f2780d332d74de668825067d6c
Kameron Ashley <kameronashley@gmail.com>
2021-10-27 15:40:57 -0700

    botched update

--- phase2-project/src/UserCard.js
+++ phase2-project/src/UserCard.js
@@ -1,13 +1,19 @@
-
+import {useState} from 'react'
 
 
 function UserCard ({author , bio , image}) {
+
+    const [showBio , setShowBio] = useState(false)
+
+    function handleBioClick () {
+        setShowBio((current) => !current)
+    }
     return(
-        <div>
+        <div onClick={handleBioClick}>
             <h2>Author: {author}</h2>
             <h2>Profile Pic: </h2>
             <img src = {image} />
-            <h3>Bio: {bio}</h3>
+            {showBio ? <h3>Bio: {bio}</h3> : null}
             
         </div>
     )
