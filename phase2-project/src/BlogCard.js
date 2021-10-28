2fa5ec0a7f5a48aa8c257d7eafa0999921cc9948
Kameron Ashley <kameronashley@gmail.com>
2021-10-28 10:19:59 -0700

    started styling

--- phase2-project/src/BlogCard.js
+++ phase2-project/src/BlogCard.js
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import styled from 'styled-components'
 
 function BlogCard ({author , title, blogpost}) {
 
@@ -9,13 +10,29 @@ function BlogCard ({author , title, blogpost}) {
     }
 
     return(
-        <div onClick={handleBlogClick}>
+        
+        <Card onClick={handleBlogClick}>
             <h1>{title}</h1>
             <h3> Author: {author} </h3>
             {showBlog ? <h4>Blog <p>{blogpost}</p> </h4>
             : null }
-        </div>
+        </Card>
     )
 };
 
 export default BlogCard
+
+
+const Card = styled.div`
+    box-shadow: 4px 4px 8px 10px rgba(0,0,0,0.2);
+    margin: 30px;
+    border: 40px;
+    width: 500px;
+    height: 160px;
+    border-radius: 8px;
+    h1,h3,h4{
+        text-align: center;
+    }
+
+
+`
